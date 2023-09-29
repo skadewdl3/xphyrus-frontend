aab510978e833b2c8e47a9b0942d2c5038b22a40
Soham Karandikar <sohamk10@gmail.com>
2023-09-29 13:02:17 +0530

    Added comments for editor.vue

--- components/Editor.vue
+++ components/Editor.vue
@@ -2,30 +2,53 @@
 import { VAceEditor } from 'vue3-ace-editor'
 import { config, edit } from 'ace-builds'
 
+// The content of the code editor
 const content = ref('')
+
+// Used to adapt the editor options background color
+// to the current theme
 const backgroundColor = ref('#fff')
 
+// Used to correctly size the editor
 const height = ref(0)
 
+// Get the editor options from the store
 const store = useEditorStore()
+
+// Have to convert the objects to refs
+// for reactive updates
 const { mode, fontSize, theme } = toRefs(store)
+
+// Use these setter functions to update the store
+// as they update the local storage as well
 const { setTheme, setMode, setFontSize } = store
 
 
-
-
-
 onMounted(async () => {
+	// Get the ace code editor element from the DOM
 	let el = document.querySelector('.ace_editor')
-	if (!el) return
+	if (!el) return // Exit if code editor fails to load
+
+	// Load the theme and mode from local storage
+	// Dynamic import these modules
 	let modeModule = await modesArray[mode.value].module
 	let themeModule = await themesArray[theme.value].module
+
+	// Set the code editor to use the improted theme and mode
 	config.setModuleUrl(`ace/mode/${modesArray[mode.value].name}`, modeModule)
 	config.setModuleUrl(`ace/theme/${themesArray[theme.value].name}`, themeModule)
+	
+	// Change background color of editor options panel
+	// to match the theme
 	backgroundColor.value = getComputedStyle(el).backgroundColor
+	
+	// Set the height to the height of the editor options panel
+	// This is used to calculate correct height for the code editor
 	height.value = (document.querySelector('.editor-options')?.clientHeight || 0)
 })
 
+// Whenever user selects new theme
+// Import the theme module and set the theme
 watch(theme, async () => {
 	let name = theme.value
 	if (!(name in themesArray)) return
@@ -38,6 +61,8 @@ watch(theme, async () => {
 	).backgroundColor
 })
 
+// Whenever user selects new language mode
+// Import the theme module and set the theme
 watch(mode, async () => {
 	let name = mode.value
 	if (!(name in modesArray)) return
@@ -54,10 +79,15 @@ watch(mode, async () => {
 		:style="`background: ${backgroundColor};  border-top-right-radius: 0.5rem; border-top-left-radius: 0.5rem`"
 	>
 		<div class="editor-options-left flex items-center">
+
+			<!-- Dropdown for language mode selection -->
+
 			<Dropdown :open="false" class="mr-2">
 				<template #name>
 					<Button :text="modesArray[mode].name" secondary />
 				</template>
+
+				<!-- Dynamically render all mode options from themesArray -->
 				<template #items>
 					<div
 						class="px-4 py-2 bg-white hover:bg-gray-200 cursor-pointer"
@@ -68,10 +98,15 @@ watch(mode, async () => {
 					</div>
 				</template>
 			</Dropdown>
+
+			<!-- Dropdown for theme selection -->
+
 			<Dropdown :open="false" class="mr-2">
 				<template #name>
 					<Button :text="themesArray[theme].name" secondary />
 				</template>
+
+				<!-- Dynamically render all theme options from themesArray -->
 				<template #items>
 					<div
 						class="px-4 py-2 bg-white hover:bg-gray-200 cursor-pointer"
@@ -83,6 +118,9 @@ watch(mode, async () => {
 				</template>
 			</Dropdown>
 		</div>
+
+		<!-- Buttons for controlling the font size of the editor -->
+		
 		<div class="editor-options-right flex items-center">
 			
 			<Button text="-" class="mr-2" @click="setFontSize(fontSize - 2)" secondary />
@@ -90,6 +128,13 @@ watch(mode, async () => {
 			
 		</div>
 	</div>
+
+	<!--
+		The code editor itself. 
+		It's height is calculated dynamically as per height of editor-options div.
+		The font-size is set dynamically as per value in the store.
+		ID of ace_editor has to be specified maually for the config utilities to work
+	-->
 	<VAceEditor
 		v-model:value="content"
 		:lang="mode"
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
+// Import the rocket image
 import heroImage from '@assets/images/hero.svg'
-const { count, setCount } = toRefs(useTestStore())
 
+// This page should use the layouts/index.vue layout
 definePageMeta({
   layout: 'index',
 })
@@ -11,19 +12,20 @@ definePageMeta({
 
   <div class="hero text-center font-display">
     <div class="hero-image">
+      <!-- Display the hero image -->
       <img :src="heroImage" class="inline-block w-[95%] sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-2/5" alt="Hero Image">
     </div>
     <div class="hero-text mt-12">
+      <!-- Display the page title -->
       <h1 class="title lg:text-5xl sm:text-4xl text-3xl">Welcome to <span class="text-primary">.xphyrus</span></h1>
+      <!-- Display the page subtitle -->
       <h3 class="subtitle md:text-lg text-md  mt-2">A Microservice based OA platform</h3>
     </div>
     <div class="hero-cta inline-flex items-center justify-center mt-8">
+      <!-- Display the login button -->
       <Button text="Login" class="mr-2" animate />
+      <!-- Display the register button -->
       <Button text="Register" animate />
     </div>
   </div>
-
 </template>
-
-<style lang="stylus">
-</style>
