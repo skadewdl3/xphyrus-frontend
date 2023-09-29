c51efdfe8671f380d07b9f9e0b89b5f23a302672
Soham Karandikar <sohamk10@gmail.com>
2023-09-29 13:09:46 +0530

    Added comments for button.vue

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 
+/**
+ * Props:
+ * @prop {String} text        - The text to display on the button
+ * @prop {Boolean} primary    - Whether the button is primary or not
+ * @prop {Boolean} secondary  - Whether the button is secondary or not
+ * @prop {Boolean} animate    - Whether the button should animate slightly down on click or not
+ * 
+ */
+
 const props = defineProps({
   text: {
     type: String,
@@ -22,6 +31,10 @@ const props = defineProps({
 </script>
 
 <template>
+
+  <!-- Render a secondary button is secondary prop is true -->
   <button v-if="props.secondary" class="bg-transparent border-solid border-[1px] border-primary px-4 py-2 rounded text-primary transition-all" :class="{'active:translate-y-0.5': props.animate}">{{ props.text }}</button>
+  
+  <!-- Render a primary button by default -->
   <button v-else-if="props.primary" class="bg-primary px-4 py-2 rounded text-white hover:bg-white border-solid border-primary border-[1px] hover:text-primary transition-all" :class="{'active:translate-y-0.5': props.animate}">{{ props.text }}</button>
 </template>
