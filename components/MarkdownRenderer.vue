c272f7e3c776072847dd640a4d4699007452841f
Soham Karandikar <sohamk10@gmail.com>
2023-10-01 20:31:18 +0530

    Added markdown rendering with syntax highlighting

--- components/MarkdownRenderer.vue
+++ components/MarkdownRenderer.vue
@@ -1,23 +1,51 @@
 <script lang="ts" setup>
-import VueMarkdown from 'vue-markdown-render'
+// import VueMarkdown from 'vue-markdown-render'
+import VueMarkdown from 'vue3-markdown-it'
+import highlightPlugin from 'markdown-it-highlightjs'
+
 import darkStylesheet from 'github-markdown-css/github-markdown-dark.css?inline'
 import lightStylesheet from 'github-markdown-css/github-markdown-light.css?inline'
 
+import highlightDark from 'highlight.js/styles/github-dark.css?inline'
+import highlightLight from 'highlight.js/styles/github.css?inline'
+
 const { theme } = toRefs(useEditorStore())
 
-watch(theme, () => {
-  // let stylesheet = theme.value == 'twilight' ? darkStylesheet : lightStylesheet
-  // document.head.appendChild()
-  
-  let existingTag = document.querySelector('.markdown-stylesheet')
-  let innerHTML = theme.value == 'twilight' ? darkStylesheet : lightStylesheet
-  if (existingTag) existingTag.innerHTML = innerHTML
-  else {
-    let styleTag = document.createElement('style')
-    styleTag.classList.add('markdown-stylesheet')
-    styleTag.innerHTML = innerHTML
-    document.head.appendChild(styleTag)
-  }
+const plugins = [
+	{
+		plugin: highlightPlugin,
+	},
+]
+
+const updateStyles = () => {
+	console.log('hi mom')
+	let stylesheetTag = document.querySelector('.markdown-stylesheet')
+	let highlightTag = document.querySelector('.highlight-stylesheet')
+
+	let innerHTML = theme.value == 'twilight' ? darkStylesheet : lightStylesheet
+	let highlightInnerHTML =
+		theme.value == 'twilight' ? highlightDark : highlightLight
+
+	if (stylesheetTag) stylesheetTag.innerHTML = innerHTML
+	else {
+		let styleTag = document.createElement('style')
+		styleTag.classList.add('markdown-stylesheet')
+		styleTag.innerHTML = innerHTML
+		document.head.appendChild(styleTag)
+	}
+	if (highlightTag) highlightTag.innerHTML = highlightInnerHTML
+	else {
+		let styleTag = document.createElement('style')
+		styleTag.classList.add('highlight-stylesheet')
+		styleTag.innerHTML = highlightInnerHTML
+		document.head.appendChild(styleTag)
+	}
+}
+
+watch(theme, updateStyles)
+
+onMounted(() => {
+	updateStyles()
 })
 
 const md = `
@@ -29,9 +57,14 @@ console.log('Hi mom')
 \`\`\`
 `
 const markdown = ref(md)
-
 </script>
 
 <template>
-  <VueMarkdown class="markdown-body bg-white dark:bg-black" :source="markdown" />
+	<ClientOnly>
+		<VueMarkdown
+			:plugins="plugins"
+			class="markdown-body bg-white dark:bg-black"
+			:source="markdown"
+		/>
+	</ClientOnly>
 </template>
